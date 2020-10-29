547fd5e2ee4e0982892fd0cf203b549bb1f15ff8
linbingjian <linbingjian@xyd.cn>
2020-10-29 23:03:18 +0800

    feat: 学生端退出登陆Bug修复& 管理端集成框架

---
+++ cms/static/manage/precache-manifest.9b2eb859bb403112cb53a240583c3291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd730c42142cb5015e339041aecd8e29",
+    "url": "/static/manage/index.html"
+  },
+  {
+    "revision": "5768518d526eee919c44",
+    "url": "/static/manage/static/css/main.389843e7.chunk.css"
+  },
+  {
+    "revision": "fc7fb4c0437b6022b47a",
+    "url": "/static/manage/static/js/2.40cbecd1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/manage/static/js/2.40cbecd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5768518d526eee919c44",
+    "url": "/static/manage/static/js/main.7a1bcd69.chunk.js"
+  },
+  {
+    "revision": "948631554fc68963a03d",
+    "url": "/static/manage/static/js/runtime-main.f5742b28.js"
+  }
+]);
