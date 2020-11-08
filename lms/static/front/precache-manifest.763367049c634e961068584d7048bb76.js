503fc47db7b664c2b4f7f8a6ad81a735d3e6c98f
linbingjian <linbingjian@xyd.cn>
2020-11-08 20:07:01 +0800

    feat: add 前后端分离框架

---
+++ lms/static/front/precache-manifest.763367049c634e961068584d7048bb76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90349bc725472dcb333391997b684a69",
+    "url": "/static/front/index.html"
+  },
+  {
+    "revision": "3024a57a9bc700991898",
+    "url": "/static/front/static/css/main.b7d834e5.chunk.css"
+  },
+  {
+    "revision": "405da42ad142c70a0b64",
+    "url": "/static/front/static/js/2.000c2c97.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/front/static/js/2.000c2c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3024a57a9bc700991898",
+    "url": "/static/front/static/js/main.73c792cc.chunk.js"
+  },
+  {
+    "revision": "f14a2c3f6584a51aa825",
+    "url": "/static/front/static/js/runtime-main.6a2d7d12.js"
+  }
+]);
