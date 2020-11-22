13fa83f89a017f1f7e3d714db0bfb207bc8035e1
linbingjian <linbingjian@xyd.cn>
2020-11-22 15:50:00 +0800

    feat: 我的学员联调

---
+++ cms/static/manage/precache-manifest.a450341c7beb1284ca5067f10f2419ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90b63b475eb60cfbca22fb6864f055d",
+    "url": "/static/studio/manage/index.html"
+  },
+  {
+    "revision": "0feabb954e1269cb1645",
+    "url": "/static/studio/manage/static/css/2.569862a8.chunk.css"
+  },
+  {
+    "revision": "ae47ad382f37fabc73fe",
+    "url": "/static/studio/manage/static/css/main.55c334f4.chunk.css"
+  },
+  {
+    "revision": "0feabb954e1269cb1645",
+    "url": "/static/studio/manage/static/js/2.11bec44c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/studio/manage/static/js/2.11bec44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae47ad382f37fabc73fe",
+    "url": "/static/studio/manage/static/js/main.b1b2a171.chunk.js"
+  },
+  {
+    "revision": "e1149224ff4ee0239bd4",
+    "url": "/static/studio/manage/static/js/runtime-main.507e1048.js"
+  }
+]);
