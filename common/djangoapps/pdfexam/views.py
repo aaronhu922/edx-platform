20af3e6d983a97c89882bd7838d5ada75c4652a5
Yong Hu <huyong922@gmail.com>
2020-12-03 17:28:07 +0800

    Add phone number field for importing pdf test result.

--- common/djangoapps/pdfexam/views.py
+++ common/djangoapps/pdfexam/views.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -27,16 +29,19 @@ def upload_file(request):
     return render(request, 'pdf2MySQL/upload_file.html')
 
 
-
+@csrf_exempt
 def Handle(request):
     # request.Files['myfile']
+    if 'phone_number' not in request.POST:
+        return HttpResponse("No phone_number for pdf importing!")
+
+    phonenumber = request.POST['phone_number']
+    log.warning(phonenumber)
+
     if request.method == 'POST':  # 请求方法为POST时，进行处理
         myFile = request.FILES['myfile']  # 获取上传的文件，如果没有文件，则默认为None
         if not myFile:
             return HttpResponse("no files for upload!")
-        phonenumber = request.data.get('phone_number')
-        if not phonenumber:
-            return HttpResponse("Phone number must be provided!")
 
         destination = open(os.path.join(BASE_DIR, 'pdfsource', myFile.name), 'wb+')  # 打开特定的文件进行二进制的写操作
 
