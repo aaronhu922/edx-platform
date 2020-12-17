b84d83372a004589aaa3c779d3f01b7ec06aeda7
linbingjian-20 <linbingjian2020@163.com>
2020-12-17 20:22:58 +0800

    feat: add reset

---
+++ lms/static/front/precache-manifest.5cc9f616b7af460912fc2353beaefd54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751bb0b171aee7b9f7ebbbcad22db436",
+    "url": "/static/front/index.html"
+  },
+  {
+    "revision": "821ed6c75f717d442a29",
+    "url": "/static/front/static/css/2.c6aee8d7.chunk.css"
+  },
+  {
+    "revision": "9a20095f74b0a9efa725",
+    "url": "/static/front/static/css/main.c27a7936.chunk.css"
+  },
+  {
+    "revision": "821ed6c75f717d442a29",
+    "url": "/static/front/static/js/2.643ebe60.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/front/static/js/2.643ebe60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a20095f74b0a9efa725",
+    "url": "/static/front/static/js/main.2308788b.chunk.js"
+  },
+  {
+    "revision": "b3827df921c0ef48814e",
+    "url": "/static/front/static/js/runtime-main.5d2812f9.js"
+  },
+  {
+    "revision": "d3e9dcfe44a47e062e49781a8744b551",
+    "url": "/static/front/static/media/login_bg.d3e9dcfe.png"
+  }
+]);
