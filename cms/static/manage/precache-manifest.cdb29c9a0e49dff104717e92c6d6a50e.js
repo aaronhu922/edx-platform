6d0db5b9e59946fad9dd70711d0be4f4b785e7c3
linbingjian-20 <linbingjian2020@163.com>
2021-01-22 21:55:01 +0800

    fix: 文案优化&下载PDF

---
+++ cms/static/manage/precache-manifest.cdb29c9a0e49dff104717e92c6d6a50e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1014ea2fa960efdb366731b5965a1682",
+    "url": "/static/studio/manage/index.html"
+  },
+  {
+    "revision": "34031ea942bd93998fcd",
+    "url": "/static/studio/manage/static/css/2.c45f52bc.chunk.css"
+  },
+  {
+    "revision": "d3b44273f804130456cb",
+    "url": "/static/studio/manage/static/css/main.7c9311f3.chunk.css"
+  },
+  {
+    "revision": "34031ea942bd93998fcd",
+    "url": "/static/studio/manage/static/js/2.36e0ae08.chunk.js"
+  },
+  {
+    "revision": "8f3a57f184c23f346c977b8d27996d32",
+    "url": "/static/studio/manage/static/js/2.36e0ae08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b44273f804130456cb",
+    "url": "/static/studio/manage/static/js/main.5d8f05a7.chunk.js"
+  },
+  {
+    "revision": "e1149224ff4ee0239bd4",
+    "url": "/static/studio/manage/static/js/runtime-main.507e1048.js"
+  }
+]);
