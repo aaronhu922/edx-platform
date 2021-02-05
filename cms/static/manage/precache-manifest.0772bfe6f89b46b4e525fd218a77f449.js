7eabca50f118c21a5c24526abc4fb88f1f2c5816
linbingjian <linbingjian@xyd.cn>
2021-02-05 21:04:33 +0800

    fix: pdf 展示

---
+++ cms/static/manage/precache-manifest.0772bfe6f89b46b4e525fd218a77f449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da68062d00e31f8e85e4e3e712e2f67",
+    "url": "/static/studio/manage/index.html"
+  },
+  {
+    "revision": "7908ce176afe4a707002",
+    "url": "/static/studio/manage/static/css/2.8b040571.chunk.css"
+  },
+  {
+    "revision": "c10e7f842fbc5507c0bc",
+    "url": "/static/studio/manage/static/css/main.ee5d4104.chunk.css"
+  },
+  {
+    "revision": "7908ce176afe4a707002",
+    "url": "/static/studio/manage/static/js/2.24a2aa29.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/studio/manage/static/js/2.24a2aa29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10e7f842fbc5507c0bc",
+    "url": "/static/studio/manage/static/js/main.1772c09a.chunk.js"
+  },
+  {
+    "revision": "e1149224ff4ee0239bd4",
+    "url": "/static/studio/manage/static/js/runtime-main.507e1048.js"
+  }
+]);
