0f9648ee15743d88814b48a8aee33a68cbcda001
linbingjian-20 <linbingjian2020@163.com>
2021-03-07 21:17:24 +0800

    fix: 管理端分页

---
+++ cms/static/manage/precache-manifest.ef869e2221f1cef1cc01befa1662310d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe09190cfa69bacedeb623e619aae7b",
+    "url": "/static/studio/manage/index.html"
+  },
+  {
+    "revision": "372fe007ed9f240e17a4",
+    "url": "/static/studio/manage/static/css/2.0fe412de.chunk.css"
+  },
+  {
+    "revision": "393d2ee37180eb84fb98",
+    "url": "/static/studio/manage/static/css/main.494dfcd5.chunk.css"
+  },
+  {
+    "revision": "372fe007ed9f240e17a4",
+    "url": "/static/studio/manage/static/js/2.775885ba.chunk.js"
+  },
+  {
+    "revision": "8f3a57f184c23f346c977b8d27996d32",
+    "url": "/static/studio/manage/static/js/2.775885ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393d2ee37180eb84fb98",
+    "url": "/static/studio/manage/static/js/main.9e2be2a5.chunk.js"
+  },
+  {
+    "revision": "e1149224ff4ee0239bd4",
+    "url": "/static/studio/manage/static/js/runtime-main.507e1048.js"
+  }
+]);
--- cms/static/manage/service-worker.js
+++ cms/static/manage/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/studio/manage/precache-manifest.c46570fef5d24b523d96cc2847d6289c.js"
+  "/static/studio/manage/precache-manifest.ef869e2221f1cef1cc01befa1662310d.js"
 );
 
 self.addEventListener('message', (event) => {
