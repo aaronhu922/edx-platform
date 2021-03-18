0348dbe67cef0a8852e23fc9661aa77064dec8ce
linbingjian-20 <linbingjian2020@163.com>
2021-03-18 23:18:27 +0800

    feat: add startreading

---
+++ cms/static/manage/precache-manifest.0bc85830707b3b702fea45f6d4c1b536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbe4f5d635af10fbd9058dcbd7bfa0c",
+    "url": "/static/studio/manage/index.html"
+  },
+  {
+    "revision": "a2834bd9d65e56fcae98",
+    "url": "/static/studio/manage/static/css/2.ce46cad6.chunk.css"
+  },
+  {
+    "revision": "8ae6cae76926abf4783a",
+    "url": "/static/studio/manage/static/css/main.d38a4c77.chunk.css"
+  },
+  {
+    "revision": "a2834bd9d65e56fcae98",
+    "url": "/static/studio/manage/static/js/2.944a3db3.chunk.js"
+  },
+  {
+    "revision": "8f3a57f184c23f346c977b8d27996d32",
+    "url": "/static/studio/manage/static/js/2.944a3db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae6cae76926abf4783a",
+    "url": "/static/studio/manage/static/js/main.b180488c.chunk.js"
+  },
+  {
+    "revision": "e1149224ff4ee0239bd4",
+    "url": "/static/studio/manage/static/js/runtime-main.507e1048.js"
+  }
+]);
